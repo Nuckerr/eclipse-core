ae214011b137a113aac4681453421314d10f71d3
Nucker <nuckermail@gmail.com>
2021-10-20 20:47:09 +0100

    Added stats command (needs testing)

--- src/main/java/gg/eclipsemc/eclipsecore/module/essentials/command/StatsCommands.java
+++ src/main/java/gg/eclipsemc/eclipsecore/module/essentials/command/StatsCommands.java
@@ -15,7 +15,7 @@ public class StatsCommands {
 
     @CommandMethod("playtime")
     @CommandDescription("View how long you have speant on the server")
-    @CommandPermission("eclipsemc.ecssentials.playtime")
+    @CommandPermission("eclipsecore.ecssentials.playtime")
     public void onPlaytime(EclipsePlayer player) {
         int playTime = player.getBukkitPlayer().getStatistic(Statistic.PLAY_ONE_MINUTE);
         player.sendMessage(Component.text("Your playtime is:").color(NamedTextColor.GOLD)
@@ -24,4 +24,16 @@ public class StatsCommands {
     }
 
     //TODO: Stats command includes last login date, first login date
+
+    @CommandMethod("stats")
+    @CommandDescription("View your stats")
+    @CommandPermission("eclipsecore.essentials.stats")
+    public void onStats(EclipsePlayer player) {
+        Component message = Component.text("Join date: ").color(NamedTextColor.YELLOW).
+                append(Component.text(player.getPlayerData().getString("firstJoin"))).color(NamedTextColor.GOLD)
+                .append(Component.newline())
+                .append(Component.text("Play time: ")).color(NamedTextColor.YELLOW)
+                .append(Component.text(player.getBukkitPlayer().getStatistic(Statistic.PLAY_ONE_MINUTE))).color(NamedTextColor.GOLD);
+        player.sendMessage(message);
+    }
 }
